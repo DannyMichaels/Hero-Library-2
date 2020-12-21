3df9e16783ff7d6309fa60cb442d516d0358da64
Daniel <itzdanielmichael@gmail.com>
2020-12-21 01:33:44 -0500

    clean comments

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Handler for .ready() called.
 $(document).ready(function () {
   const domain =
     "https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/";
-  // https://cors-anywhere.herokuapp.com/
+
   const apiKey = "1791582447662011";
   const baseUrl = `${domain}${apiKey}/search/`;
 
-  const hamburger = $("#hamburger"); //selecting the hamburger and tabs-ul id with $ instead of ES6 query selector.
-  const tabsUl = $("#tabs-ul"); //selecting the hamburger and tabs-ul id with $ instead of ES6 query selector.
+  const hamburger = $("#hamburger");
+  const tabsUl = $("#tabs-ul");
 
-  // .on is a jQuery method
-  // https://api.jquery.com/on/#on-events-selector-data-handler
   hamburger.on("click", () => {
-    // https://api.jquery.com/toggleclass/
     tabsUl.toggleClass("show");
   });
 
@@ -23,38 +19,25 @@ $(document).ready(function () {
   const input = $("#search-hero-input");
   const button = $("#search-hero-button");
 
-  // https://api.jquery.com/click/#click
   button.click(async (e) => {
-    // this event listener listens to the submit button click.
-    e.preventDefault(); // prevents page from reloading once search submitted
-    //https://api.jquery.com/val/#val
-    let userInput = input.val(); // takes user input
+    e.preventDefault();
+    let userInput = input.val();
     const res = await axios.get(`${baseUrl}${userInput}`);
     const response = res.data.results;
     const movieRes = await axios.get(`${omDBurl}s=${userInput}`);
-
-    // Shay and Misha helped me with this
-    res.data.response === "error" ? null : renderList(movieRes.data.Search); // if no hero search results appear, do not return movie results.
-
+    res.data.response === "error" ? null : renderList(movieRes.data.Search);
     removeHero();
-
     const heroesDiv = $(".heroes");
-
     let heroData = $(`<h3 class="hero-data">Hero Data:</h3>`);
-    // ES6 equivalent: document.getElementById('main-title').innerHTML = '<p class="hero-detail">some text</p>'
-    // "<p class="hero-detail">some text</p>"
 
     heroesDiv.append(heroData);
 
     if (res.data.response === "error") {
-      // if no search results found, give an alert message saying "no hero found"
       heroData.html("NO HERO FOUND!");
       alert("No hero found, try a different name!");
     }
 
     response.forEach((hero) => {
-      // looping through array
-
       const heroBio = hero.biography;
       const heroAppearance = hero.appearance;
       const heroStats = hero.powerstats;
@@ -84,29 +67,19 @@ $(document).ready(function () {
   });
 
   function removeHero() {
-    // This function removes heroes once a new search has been submitted
     $(".heroes").html("");
   }
 
-  const movieDisplay = $(".movies"); // select the class .movies and assign it to movieDisplay
+  const movieDisplay = $(".movies");
 
   const renderList = (movies) => {
-    let names1 = $("<h3></h3>"); // assign h3 to variable names1
-
-    //https://api.jquery.com/attr/
-    names1.attr("class", "names-movie"); // give names1 h3 a class name
-
-    // https://api.jquery.com/html/
-    names1.html("Movie/Series:"); // give names1 text "Movie/Series:"
-
-    // https://www.w3schools.com/jquery/jquery_dom_add.asp
-    movieDisplay.append(names1); // append the h3 to the movie
-
+    let names1 = $("<h3></h3>");
+    names1.attr("class", "names-movie");
+    names1.html("Movie/Series:");
+    movieDisplay.append(names1);
     movies.forEach((movie) => {
-      // looping through the array
-      const movieContainer = $("<div></div>"); // create div element and assign it to a variable
+      const movieContainer = $("<div></div>");
 
-      // https://api.jquery.com/addClass/#addClass-className
       movieContainer.addClass("movie-container"); // give it a class and a class name
 
       const title = $("<h3></h3>"); // creating h3 for movie title
@@ -138,3 +111,5 @@ $(document).ready(function () {
     $(".movies").html("");
   }
 });
+
+// more comments in the develop branch
